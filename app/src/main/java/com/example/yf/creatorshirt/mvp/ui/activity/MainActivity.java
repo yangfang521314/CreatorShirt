21cbde1d3caf7f1a2c362f6c7abad8e1e2dc70e6
yangfang521314 <504864481@qq.com>
2017-07-06 17:44:37 +0800

    首页图标点击的变化

--- app/src/main/java/com/example/yf/creatorshirt/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/yf/creatorshirt/mvp/ui/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.yf.creatorshirt.mvp.ui.activity;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
@@ -14,16 +17,17 @@ import com.example.yf.creatorshirt.mvp.ui.fragment.HomeFragment;
 import butterknife.BindView;
 import butterknife.OnClick;
 
+@TargetApi(Build.VERSION_CODES.M)
 public class MainActivity extends BaseActivity {
-    private static final String TYPE_COMMUNITY = "community";
-    private static final String TYPE_CREATE = "create";
-    private static final String TYPE_HOME = "home";
-    @BindView(R.id.home_text)
-    TextView mCommunityText;
-    @BindView(R.id.create_text)
-    TextView mCreateText;
-    @BindView(R.id.my_text)
-    TextView mMyText;
+    private static final String TYPE_SQUARE = "square";
+    private static final String TYPE_DESIGN = "design";
+    private static final String TYPE_MINE = "mine";
+    @BindView(R.id.square_text)
+    TextView mSquareContainer;
+    @BindView(R.id.design_text)
+    TextView mDesignContainer;
+    @BindView(R.id.mine_text)
+    TextView mMineContainer;
     @BindView(R.id.tool_bar)
     Toolbar mToolbar;
 
@@ -43,29 +47,69 @@ public class MainActivity extends BaseActivity {
     protected void initView() {
         mCommunityFragment = new CommunityFragment();
         mHomeFragment = new HomeFragment();
-        showFragment = TYPE_COMMUNITY;
-        hideFragment = TYPE_COMMUNITY;
+        showFragment = TYPE_SQUARE;
+        hideFragment = TYPE_SQUARE;
         mTransaction = getSupportFragmentManager().beginTransaction();
         mTransaction.add(R.id.content, mCommunityFragment, "community").show(mCommunityFragment)
                 .add(R.id.content, mHomeFragment, "home").hide(mHomeFragment).commit();
         mToolbar.setTitle(null);
     }
 
-    @OnClick({R.id.home_text, R.id.create_text, R.id.my_text})
+    @OnClick({R.id.mine_text, R.id.design_text, R.id.square_text})
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.home_text:
-                showFragment = TYPE_COMMUNITY;
+            case R.id.square_text:
+                showFragment = TYPE_SQUARE;
+                choiceTabState(TYPE_SQUARE);
                 break;
-            case R.id.create_text:
+            case R.id.design_text:
                 startActivity(new Intent(this, DesignActivity.class));
+                choiceTabState(TYPE_DESIGN);
                 break;
-            case R.id.my_text:
-                showFragment = TYPE_HOME;
+            case R.id.mine_text:
+                showFragment = TYPE_MINE;
+                choiceTabState(TYPE_MINE);
                 break;
         }
         changeFragment(getShowFragment(showFragment), getShowFragment(hideFragment));
         hideFragment = showFragment;
+
+    }
+
+    private void choiceTabState(String position) {
+        clearSelection();
+        switch (position) {
+            case TYPE_SQUARE:
+                Drawable drawable01 = getResources().getDrawable(R.mipmap.icon_square_hover, null);
+                mSquareContainer.setCompoundDrawablesRelativeWithIntrinsicBounds(null, drawable01, null, null);
+                mSquareContainer.setTextColor(getResources().getColor(R.color.sand, null));
+                break;
+            case TYPE_DESIGN:
+                Drawable drawable02 = getResources().getDrawable(R.mipmap.icon_design_hover, null);
+                mDesignContainer.setCompoundDrawablesRelativeWithIntrinsicBounds(null, drawable02, null, null);
+                mDesignContainer.setTextColor(getResources().getColor(R.color.sand, null));
+                break;
+            case TYPE_MINE:
+                Drawable drawable03 = getResources().getDrawable(R.mipmap.icon_mine_hover, null);
+                mMineContainer.setCompoundDrawablesRelativeWithIntrinsicBounds(null, drawable03, null, null);
+                mMineContainer.setTextColor(getResources().getColor(R.color.sand, null));
+                break;
+        }
+    }
+
+    //清除上次的状态
+    private void clearSelection() {
+        Drawable drawable01 = getResources().getDrawable(R.mipmap.icon_square_normal, null);
+        mSquareContainer.setCompoundDrawablesRelativeWithIntrinsicBounds(null, drawable01, null, null);
+        mSquareContainer.setTextColor(getResources().getColor(R.color.manatee_1, null));
+
+        Drawable drawable02 = getResources().getDrawable(R.mipmap.icon_design_normal, null);
+        mDesignContainer.setCompoundDrawablesRelativeWithIntrinsicBounds(null, drawable02, null, null);
+        mDesignContainer.setTextColor(getResources().getColor(R.color.manatee_1, null));
+
+        Drawable drawable03 = getResources().getDrawable(R.mipmap.icon_mine_normal, null);
+        mMineContainer.setCompoundDrawablesRelativeWithIntrinsicBounds(null, drawable03, null, null);
+        mMineContainer.setTextColor(getResources().getColor(R.color.manatee_1, null));
 
     }
 
@@ -78,9 +122,9 @@ public class MainActivity extends BaseActivity {
 
     private Fragment getShowFragment(String flag) {
         switch (flag) {
-            case TYPE_COMMUNITY:
+            case TYPE_SQUARE:
                 return mCommunityFragment;
-            case TYPE_HOME:
+            case TYPE_MINE:
                 return mHomeFragment;
         }
         return mCommunityFragment;
